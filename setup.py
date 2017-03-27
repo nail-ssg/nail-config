cc2aec5f4357a8ac24639e0c309cec77ffce7e03
Nail Gimaev <gimntut@list.ru>
2017-03-27 16:01:50 +0500

    -

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='nail.config',
-    version='0.1.0',
+    version='0.1.001',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     install_requires=[
