634e4560a5bd4dc32428f15806f1725fa97d5280
Nail Gimaev <gimntut@list.ru>
2017-03-29 11:56:02 +0500

    Fix version

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from os.path import join, dirname
 
 setup(
     name='nail.config',
-    version='0.1.002',
+    version='0.1.2',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     install_requires=[
-        'ruamel.yaml==0.13.14'
+        'ruamel.yaml>=0.13.14'
     ])
