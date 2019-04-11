8104f296b06b56f6b09892bd3557653419d756c6
Гимаев Наиль <gimntut@list.ru>
2019-04-12 01:09:54 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     install_requires=[
-        'ruamel.yaml==0.13.14'
+        'ruamel.yaml>=0.13.14'
     ])
